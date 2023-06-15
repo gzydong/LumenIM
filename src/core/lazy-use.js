cd0f27e12ef613439d024c733cc10783b4b26043
yuandong <837215079@qq.com>
2023-06-15 21:12:20 +0800

    feat:优化代码

--- src/core/lazy-use.js
+++ src/core/lazy-use.js
@@ -89,7 +89,7 @@ import BaiduMap from 'vue-baidu-map'
 
 Vue.use(BaiduMap, {
   // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
-  ak: 'Q47kDECgU9ROIGiWnyXldH0hL9IVhXxt'
+  ak: ''
 })
 
 process.env.NODE_ENV !== 'production' &&
